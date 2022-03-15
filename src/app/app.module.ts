0df7e02c116253c990b5590ebffbcaece3966322
Projet collaboratif Master 1 MIAGE <87546751+aissatoubarryfab@users.noreply.github.com>
2022-03-15 15:27:14 +0100

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { PcPortableComponent } from './pc-portable/pc-portable.component';
 import { PcBureauComponent } from './pc-bureau/pc-bureau.component';
 import { PcAccessoiresComponent } from './pc-accessoires/pc-accessoires.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> acf9bf70dbc7d050fc14fe01a1f95841dcef3776
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     PcPortableComponent,
     PcBureauComponent,
     PcAccessoiresComponent,
     ConnexionComponent,
-    HomeComponent
-=======
+    HomeComponent,
     NavbarComponent
->>>>>>> acf9bf70dbc7d050fc14fe01a1f95841dcef3776
   ],
   imports: [
     BrowserModule,

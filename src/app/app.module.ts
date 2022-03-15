01afb5b7050b63398568706166b5e1b331c9727c
LydiaSM <l.smahi@hotmail.fr>
2022-03-15 15:11:36 +0100

    ajout composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PcPortableComponent } from './pc-portable/pc-portable.component';
+import { PcBureauComponent } from './pc-bureau/pc-bureau.component';
+import { PcAccessoiresComponent } from './pc-accessoires/pc-accessoires.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PcPortableComponent,
+    PcBureauComponent,
+    PcAccessoiresComponent,
+    ConnexionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

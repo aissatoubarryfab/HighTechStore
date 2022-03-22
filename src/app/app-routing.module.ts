477154ff718c657b83f4df00c47b5a9ae43b76c9
LydiaSM <l.smahi@hotmail.fr>
2022-03-22 17:51:23 +0100

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { ConnexionComponent } from './auth/login/connexion.component';
+
 import { HomeComponent } from './home/home.component';
+import { ConnexionComponent } from './auth/login/connexion.component';
+
+import { PcAccessoiresComponent } from './modules/ordinateur/pc-accessoires/pc-accessoires.component';
+import { PcBureauComponent } from './modules/ordinateur/pc-bureau/pc-bureau.component';
+import { PcPortableComponent } from './modules/ordinateur/pc-portable/pc-portable.component';
+
+import { SmartPhoneComponent } from './modules/phones/smart-phone/smart-phone.component';
+import { TelFixComponent } from './modules/phones/tel-fix/tel-fix.component';
+import { TelAccessoiresComponent } from './modules/phones/tel-accessoires/tel-accessoires.component';
+
+import { CleUSBComponent } from './modules/stockage/cle-usb/cle-usb.component';
+import { DisqueDurComponent } from './modules/stockage/disque-dur/disque-dur.component';
+import { StockageAccessoiresComponent } from './modules/stockage/stockage-accessoires/stockage-accessoires.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  // { path: 'PcPortable', component: PcPortableComponent },
-  // { path: 'PcBureau', component: PcBureauComponent },
-  // { path: 'PcAccessoires', component: PcAccessoiresComponent },
-  { path: 'Stockage', loadChildren :'./stockage/stockage.module#StockageModule' },
+  { path: 'Home', component: HomeComponent },
   { path: 'Connexion', component: ConnexionComponent },
+
+  { path: 'PcPortable', component: PcPortableComponent },
+  { path: 'PcBureau', component: PcBureauComponent },
+  { path: 'PcAccessoires', component: PcAccessoiresComponent },
+
+  { path: 'Smart', component: SmartPhoneComponent },
+  { path: 'Fixe', component: TelFixComponent },
+  { path: 'PhoneAccessoires', component: TelAccessoiresComponent },
+
+  { path: 'CleUSB', component: CleUSBComponent },
+  { path: 'DisqueDur', component: DisqueDurComponent },
+  { path: 'StockageAccesoires', component: StockageAccessoiresComponent },
+
 ];
   
 @NgModule({
